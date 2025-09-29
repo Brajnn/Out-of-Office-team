463d4ae601a3a322de4c89baab995131aab010f5
Brajnn <olido22@wp.pl>
2025-09-29 11:52:42 +0200

    Add Localizer to Authenticator pages

--- Out_of_Office/Models/EnableAuthenticatorViewModel.cs
+++ Out_of_Office/Models/EnableAuthenticatorViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Out_of_Office.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Out_of_Office.Models
 {
     public class EnableAuthenticatorViewModel
     {
         public string Key { get; set; }
+        [Required(ErrorMessage = "CodeRequiredError")]
+        [Display(Name = "CodeLabel")]
         public string Code { get; set; }
     }
 }
