5996addc497c18f0693b957962278baa8e4844a5
Brajnn <olido22@wp.pl>
2025-10-01 07:59:41 +0200

    added lozalizer to CreateApprovalRequest

--- Out_of_Office.Application/Employee/Command/CreateEmployee/CreateEmployeeCommand.cs
+++ Out_of_Office.Application/Employee/Command/CreateEmployee/CreateEmployeeCommand.cs
@@ -17,7 +17,7 @@ namespace Out_of_Office.Application.Employee.Command.CreateEmployee
         public DateTime HireDate { get; set; }
         public int PeoplePartnerID { get; set; }
         public int OutOfOfficeBalance { get; set; }
-        public byte[] Photo { get; set; }
+        public byte[]? Photo { get; set; }
 
         public int VacationDays { get; set; }
         public int SickLeaveDays { get; set; }
